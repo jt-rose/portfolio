673f0d4e0a64ef22d4ec599e1ca950e9475741f7
Jeffrey Rose <jtr219@outlook.com>
2022-01-27 13:44:15 -0500

    add onClick display for company logos

--- build/index.js
+++ build/index.js
@@ -1,4 +1,34 @@
 "use strict";
 $(function () {
-    console.log("is this thing on?");
+    /* -------------------------------------------------------------------------- */
+    /*                              get HTML elements                             */
+    /* -------------------------------------------------------------------------- */
+    var $gaLogo = $("#ga-logo");
+    var $challengerLogo = $("#challenger-logo");
+    var $bareLogo = $("#bare-logo");
+    var $gaExp = $("#ga-experience-info");
+    var $challengerExp = $("#challenger-experience-info");
+    var $bareExp = $("#bare-experience-info");
+    /* -------------------------------------------------------------------------- */
+    /*                               site functions                               */
+    /* -------------------------------------------------------------------------- */
+    //
+    /* -------------------------------------------------------------------------- */
+    /*                             add event listeners                            */
+    /* -------------------------------------------------------------------------- */
+    $gaLogo.on("click", function () {
+        $gaExp.show();
+        $challengerExp.hide();
+        $bareExp.hide();
+    });
+    $challengerLogo.on("click", function () {
+        $gaExp.hide();
+        $challengerExp.show();
+        $bareExp.hide();
+    });
+    $bareLogo.on("click", function () {
+        $gaExp.hide();
+        $challengerExp.hide();
+        $bareExp.show();
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,41 @@
 $(() => {
-  console.log("is this thing on?");
+  /* -------------------------------------------------------------------------- */
+  /*                              get HTML elements                             */
+  /* -------------------------------------------------------------------------- */
+
+  const $gaLogo = $("#ga-logo");
+  const $challengerLogo = $("#challenger-logo");
+  const $bareLogo = $("#bare-logo");
+
+  const $gaExp = $("#ga-experience-info");
+  const $challengerExp = $("#challenger-experience-info");
+  const $bareExp = $("#bare-experience-info");
+
+  /* -------------------------------------------------------------------------- */
+  /*                               site functions                               */
+  /* -------------------------------------------------------------------------- */
+
+  //
+
+  /* -------------------------------------------------------------------------- */
+  /*                             add event listeners                            */
+  /* -------------------------------------------------------------------------- */
+
+  $gaLogo.on("click", () => {
+    $gaExp.show();
+    $challengerExp.hide();
+    $bareExp.hide();
+  });
+
+  $challengerLogo.on("click", () => {
+    $gaExp.hide();
+    $challengerExp.show();
+    $bareExp.hide();
+  });
+
+  $bareLogo.on("click", () => {
+    $gaExp.hide();
+    $challengerExp.hide();
+    $bareExp.show();
+  });
 });

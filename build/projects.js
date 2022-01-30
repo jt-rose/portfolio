1ab99c3e38216c3de995959c929f3003284bec89
Jeffrey Rose <jtr219@outlook.com>
2022-01-29 21:32:16 -0500

    set up image paths

--- build/projects.js
+++ build/projects.js
@@ -1,14 +1,32 @@
 "use strict";
+/* -------------------------------------------------------------------------- */
+/*                              declare variables                             */
+/* -------------------------------------------------------------------------- */
+var rxpIMGPaths = [
+    "./assets/projects/rxp-1.png",
+    "./assets/projects/rxp-2.png",
+    "./assets/projects/rxp-3.png",
+];
+var cleanBlogIMGPaths = [
+    "./assets/projects/clean-blog-1-mobile.png",
+    "./assets/projects/clean-blog-2.png",
+    "./assets/projects/clean-blog-3.png",
+    "./assets/projects/clean-blog-4.png",
+    "./assets/projects/clean-blog-5.png",
+    "./assets/projects/clean-blog-6.png",
+];
+var _2dalooIMGPaths = [
+    "./assets/projects/2daloo-1.png",
+    "./assets/projects/2daloo-2.png",
+    "./assets/projects/2daloo-3.png",
+    "./assets/projects/2daloo-4.png",
+];
+var currentIndex = 0;
+var currentImageSet = rxpIMGPaths;
 $(function () {
-    var rxpIMGPaths = [
-        "./assets/projects/rxp-1.png",
-        "./assets/projects/rxp-2.png",
-        "./assets/projects/rxp-3.png",
-    ];
-    var currentIndex = 0;
     var updateProjectImage = function () {
         currentIndex++;
-        if (currentIndex >= rxpIMGPaths.length) {
+        if (currentIndex >= currentImageSet.length) {
             currentIndex = 0;
         }
         $("#projects-modal-sample-img").attr("src", rxpIMGPaths[currentIndex]);
--- src/projects.ts
+++ src/projects.ts
@@ -1,13 +1,36 @@
+/* -------------------------------------------------------------------------- */
+/*                              declare variables                             */
+/* -------------------------------------------------------------------------- */
+
+const rxpIMGPaths = [
+  "./assets/projects/rxp-1.png",
+  "./assets/projects/rxp-2.png",
+  "./assets/projects/rxp-3.png",
+];
+
+const cleanBlogIMGPaths = [
+  "./assets/projects/clean-blog-1-mobile.png",
+  "./assets/projects/clean-blog-2.png",
+  "./assets/projects/clean-blog-3.png",
+  "./assets/projects/clean-blog-4.png",
+  "./assets/projects/clean-blog-5.png",
+  "./assets/projects/clean-blog-6.png",
+];
+
+const _2dalooIMGPaths = [
+  "./assets/projects/2daloo-1.png",
+  "./assets/projects/2daloo-2.png",
+  "./assets/projects/2daloo-3.png",
+  "./assets/projects/2daloo-4.png",
+];
+
+let currentIndex = 0;
+let currentImageSet = rxpIMGPaths;
+
 $(() => {
-  const rxpIMGPaths = [
-    "./assets/projects/rxp-1.png",
-    "./assets/projects/rxp-2.png",
-    "./assets/projects/rxp-3.png",
-  ];
-  let currentIndex = 0;
   const updateProjectImage = () => {
     currentIndex++;
-    if (currentIndex >= rxpIMGPaths.length) {
+    if (currentIndex >= currentImageSet.length) {
       currentIndex = 0;
     }
     $("#projects-modal-sample-img").attr("src", rxpIMGPaths[currentIndex]);

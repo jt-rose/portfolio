8e599471d90f4e372aecd05d646fd7ea704d013a
Jeffrey Rose <jtr219@outlook.com>
2022-01-30 23:36:38 -0500

    transform menu icon to x upon open

--- build/header.js
+++ build/header.js
@@ -4,6 +4,12 @@ $(function () {
     var $menuIcon = $("#menu-icon");
     $menuIcon.on("click", function (e) {
         e.preventDefault();
+        if ($header.hasClass("dropdown-active")) {
+            $menuIcon.children().removeClass("fa-times").addClass("fa-bars");
+        }
+        else {
+            $menuIcon.children().removeClass("fa-bars").addClass("fa-times");
+        }
         $header.toggleClass("dropdown-active");
     });
 });
--- src/header.ts
+++ src/header.ts
@@ -4,6 +4,11 @@ $(() => {
 
   $menuIcon.on("click", (e) => {
     e.preventDefault();
+    if ($header.hasClass("dropdown-active")) {
+      $menuIcon.children().removeClass("fa-times").addClass("fa-bars");
+    } else {
+      $menuIcon.children().removeClass("fa-bars").addClass("fa-times");
+    }
     $header.toggleClass("dropdown-active");
   });
 });

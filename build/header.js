8430e1551a79fb64147fbef0ab8ba041d99705c2
Jeffrey Rose <jtr219@outlook.com>
2022-01-31 12:27:40 -0500

    animate mobile-menu to close on selection

--- build/header.js
+++ build/header.js
@@ -1,19 +1,34 @@
 "use strict";
 $(function () {
+    // get header/ navbar element and menu icon (visible on mobile)
     var $header = $("header");
     var $menuIcon = $("#menu-icon");
+    // open or close the menu based on current state
+    // and animate the transition
+    var animateMenuToggle = function () {
+        var windowWidth = $(window).width();
+        if (windowWidth && windowWidth < 610) {
+            $header.css("transition", "0.7s");
+            if ($header.hasClass("dropdown-active")) {
+                $menuIcon.children().removeClass("fa-times").addClass("fa-bars");
+            }
+            else {
+                $menuIcon.children().removeClass("fa-bars").addClass("fa-times");
+            }
+            $header.toggleClass("dropdown-active");
+            setTimeout(function () {
+                $header.css("transition", "0");
+            }, 700);
+        }
+    };
+    // apply menuToggle animation to all section links
+    $("nav a")
+        .filter(function (index) { return index !== 5; })
+        .on("click", animateMenuToggle);
+    // for menu/ cancel icon, disable the section link
+    // and only animate the menu opening and closing
     $menuIcon.on("click", function (e) {
         e.preventDefault();
-        $header.css("transition", "0.7s");
-        if ($header.hasClass("dropdown-active")) {
-            $menuIcon.children().removeClass("fa-times").addClass("fa-bars");
-        }
-        else {
-            $menuIcon.children().removeClass("fa-bars").addClass("fa-times");
-        }
-        $header.toggleClass("dropdown-active");
+        animateMenuToggle();
     });
-    setTimeout(function () {
-        $header.css("transition", "0");
-    }, 700);
 });
--- src/header.ts
+++ src/header.ts
@@ -1,18 +1,35 @@
 $(() => {
+  // get header/ navbar element and menu icon (visible on mobile)
   const $header = $("header");
   const $menuIcon = $("#menu-icon");
 
+  // open or close the menu based on current state
+  // and animate the transition
+  const animateMenuToggle = () => {
+    const windowWidth = $(window).width();
+    if (windowWidth && windowWidth < 610) {
+      $header.css("transition", "0.7s");
+      if ($header.hasClass("dropdown-active")) {
+        $menuIcon.children().removeClass("fa-times").addClass("fa-bars");
+      } else {
+        $menuIcon.children().removeClass("fa-bars").addClass("fa-times");
+      }
+      $header.toggleClass("dropdown-active");
+      setTimeout(() => {
+        $header.css("transition", "0");
+      }, 700);
+    }
+  };
+
+  // apply menuToggle animation to all section links
+  $("nav a")
+    .filter((index) => index !== 5)
+    .on("click", animateMenuToggle);
+
+  // for menu/ cancel icon, disable the section link
+  // and only animate the menu opening and closing
   $menuIcon.on("click", (e) => {
     e.preventDefault();
-    $header.css("transition", "0.7s");
-    if ($header.hasClass("dropdown-active")) {
-      $menuIcon.children().removeClass("fa-times").addClass("fa-bars");
-    } else {
-      $menuIcon.children().removeClass("fa-bars").addClass("fa-times");
-    }
-    $header.toggleClass("dropdown-active");
+    animateMenuToggle();
   });
-  setTimeout(() => {
-    $header.css("transition", "0");
-  }, 700);
 });
